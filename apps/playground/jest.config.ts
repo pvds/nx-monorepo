be0ba89c207a2ca8ccd89b5093dee01c12dc7920
pvdsteen <peter.van.der.steen@priva.com>
2022-04-30 22:59:06 +0200

    feat: create nx workspace for web components

---
+++ apps/playground/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'playground',
+  preset: '../../jest.preset.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/playground',
+};
